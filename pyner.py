3f521080e316711a7ede63799635ab806d9bf3c9
natrix_dev <discordlenko@gmail.com>
2021-12-30 00:47:20 +0100

    Create pyner.py

---
+++ pyner.py
@@ -0,0 +1,12 @@
+from port-manager import *
+
+port-manger.take(port:localhost=>8000port:webhost=>$takePorte from port)
+
+chooser from chooseChoices
+
+
+choose1: localhost;
+  console.log("Your port is the" $portNumberForLocalHost)
+  
+choose2: webhost; 
+  console.log("Your port is the" $portNumberForWebHost)
